d35ae7931ae6d2b361cc6ef5fdb958a37dcc9b83
bardrabbit <bardrabbit@gmail.com>
2021-08-05 09:04:12 -0400

    set up materialize

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css/dist/js/materialize.min.js';
 import '../css/App.css';
 
 const App = () => {
+  // init materialize js
+  useEffect(() => {
+    M.AutoInit();
+  });
   return <div>App</div>;
 };
 
