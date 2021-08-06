2e8087d6b495ede91effcbfc2e5a5bf7de0bb457
bardrabbit <bardrabbit@gmail.com>
2021-08-06 00:22:06 -0400

    add techItem to tech list modal and complete UI setting

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import AddBtn from './layout/AddBtn';
 import AddLogModal from './logs/AddLogModal';
 import EditLogModal from './logs/EditLogModal';
 import AddTechModal from './techs/AddTechModal';
+import TechListModal from './techs/TechListModal';
 import '../css/App.css';
 
 const App = () => {
@@ -22,6 +23,7 @@ const App = () => {
         <AddLogModal />
         <EditLogModal />
         <AddTechModal />
+        <TechListModal />
         <Logs />
       </div>
     </Fragment>
